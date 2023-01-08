6e2afb04f9aca18266db86fa58606b2720a6800f
kunlexi <adesinataofik5@gmail.com>
2023-01-08 07:36:44 +0100

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import Home from "@/scenes/home";
 import OurClasses from "@/scenes/ourClasses";
 import Benefits from "@/scenes/benefits";
 import ContactUs from "@/scenes/contactUs";
+import Footer from "@/scenes/footer";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
+
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
@@ -33,6 +35,7 @@ function App() {
       <Benefits setSelectedPage={setSelectedPage}/>
       <OurClasses setSelectedPage={setSelectedPage}/>
       <ContactUs setSelectedPage={setSelectedPage}/>
+      <Footer />
       
 
     </div>
--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -68,7 +68,7 @@ const ContactUs = ({ setSelectedPage }: Props) => {
             <form
               target="_blank"
               onSubmit={onSubmit}
-              action="https://formsubmit.co/e8a5bdfa807605332f809e5656e27c6e"
+              action="https://formsubmit.co/e202989e40b6325307ffb7b41d37db6b"
               method="POST"
             >
               <input
